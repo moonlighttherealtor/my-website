1a6fac304cdde4e5a88b213d222f0c38a7fcff18
Mahtab Hafezi <mahtab@Mahtabs-MacBook-Pro.local>
2025-08-23 17:04:39 -0700

    Update Open Graph image for search results

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: EstateAgency
-* Template URL: https://bootstrapmade.com/real-estate-agency-bootstrap-template/
-* Updated: Aug 09 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 
 (function() {
   "use strict";
